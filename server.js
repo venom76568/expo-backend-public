c43e7a29138d3e1c65311bf8a82976e37e5c3f68
Sanchit Padwekar <86397529+soulfaced@users.noreply.github.com>
2024-01-16 00:21:37 +0530

    cors

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ const PORT = 5000;
 // Enable CORS
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
 // Middleware to parse JSON in request body
 app.use(bodyParser.json());
 
